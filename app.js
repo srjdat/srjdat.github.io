b4506c40135abd92c710925ca1299d495a95e3fb
YeaButActuallyNo <srjdatta7@gmail.com>
2023-09-25 16:22:44 -0500

    init

---
+++ app.js
@@ -0,0 +1,38 @@
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+function PageTransitions() {
+    //button click active class 
+    for (let i = 0; i < secBtn.length; i++) {
+        secBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn'; 
+        })
+    }
+
+    //sections active class 
+    allSections[0].addEventListener('click', (e) =>{
+        const id = e.target.dataset.id; 
+        if(id) {
+            //remove selected from the other buttons
+            secBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    });
+}
+
+
+PageTransitions(); 
